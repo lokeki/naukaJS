79d6f1ef5858526d132386ec945ced87835c7ad1
Kamila Liskiewicz <kamila.liskiewicz12@gmail.com>
2020-12-05 15:39:06 +0100

    Uzupelnienie informacji o DOM metody przeszukiwania

--- DOM/przeszukiwaniedrzewaDOM/metodyPrzeszukiwaniaDrzewa.js
+++ DOM/przeszukiwaniedrzewaDOM/metodyPrzeszukiwaniaDrzewa.js
@@ -14,4 +14,22 @@ var h1 = document.querySelector('h1'); // zwraca tylko jeden element
 var form = document.querySelector('#myForm');
 var submit = form.querySelector("buttom[type='submit']"); // selektor css3
 
-var links = document.querySelectorAll('a'); //  zwraca obiekt tablicopodobny
+var links = document.querySelectorAll('div a'); //  zwraca obiekt tablicopodobny, wszytskie elementy a ktr sa w div ( tak jak w css)
+
+/* 
+Roznice miedzy getElement a querySelector:
+getElement -> 
+ - nie mozemy tu przekazywac selektorow jak np klasa, buttom[type='submit'] itd;
+ - jest 'zywa' lista, zmieniaja sie wraz ze zmianami
+
+querySelector -> 
+ - zachowuje wszystkie elementy, ktore zostaly wpisane na poczastku, nie reaguje na zmiany, nie jest 'zywa'
+
+*/
+
+// wlasciwosci:
+//obiekty tablicopodobne
+document.all; // zwroci wszystkie elementy na stronie (wychodzi)
+document.forms; // zwroci wszytskie form
+document.images; // zwroci wszytskie img
+document.links; // zwroci wszytskie a
